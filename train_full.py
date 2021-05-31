56c26d8501f1284f0129afd6b3d56b245609546f
kishanwn <kishanwn@gmail.com>
2021-05-31 20:18:24 +0900

    Update train_full.py

--- train_full.py
+++ train_full.py
@@ -3,14 +3,10 @@ from __future__ import print_function
 import time
 import random
 import argparse
-import numpy as np
-import torch
-import torch.nn.functional as F
 import torch.optim as optim
 from process import *
 from utils import *
 import uuid
-import scipy.io as sio
 from model import *
 
 # Training settings
@@ -43,10 +39,10 @@ device = torch.device(cudaid)
 checkpt_file = 'pretrained/'+uuid.uuid4().hex+'.pt'
 print(cudaid,checkpt_file)
 
-def train_step(model,optimizer,features,labels,adj,idx_train):
+def train_step(model,optimizer,features,labels,idx_train):
     model.train()
     optimizer.zero_grad()
-    output = model(features,adj)
+    output = model(features)
     acc_train = accuracy(output[idx_train], labels[idx_train].to(device))
     loss_train = F.nll_loss(output[idx_train], labels[idx_train].to(device))
     loss_train.backward()
@@ -54,19 +50,19 @@ def train_step(model,optimizer,features,labels,adj,idx_train):
     return loss_train.item(),acc_train.item()
 
 
-def validate_step(model,features,labels,adj,idx_val):
+def validate_step(model,features,labels,idx_val):
     model.eval()
     with torch.no_grad():
-        output = model(features,adj)
+        output = model(features)
         loss_val = F.nll_loss(output[idx_val], labels[idx_val].to(device))
         acc_val = accuracy(output[idx_val], labels[idx_val].to(device))
         return loss_val.item(),acc_val.item()
 
-def test_step(model,features,labels,adj,idx_test):
+def test_step(model,features,labels,idx_test):
     model.load_state_dict(torch.load(checkpt_file))
     model.eval()
     with torch.no_grad():
-        output = model(features, adj)
+        output = model(features)
         loss_test = F.nll_loss(output[idx_test], labels[idx_test].to(device))
         acc_test = accuracy(output[idx_test], labels[idx_test].to(device))
         return loss_test.item(),acc_test.item()
@@ -77,14 +73,14 @@ def train(datastr,splitstr):
     features = features.to(device)
     adj = adj.to(device)
 
-    pw = torch.zeros([gpr_coeff, adj.size(1), adj.size(1)]).to(device)
-    print(pw.size())
+    # Precompute all powers of the normalized adjacency matrix
+    adj_powers = torch.zeros([gpr_coeff, adj.size(1), adj.size(1)]).to(device)
     I = torch.eye(adj.size(1)).to(device)
-    pw[0, :, :] = I
+    adj_powers[0, :, :] = I
     for i in range(gpr_coeff-1):
-        pw[i+1, :, :] = torch.spmm(adj, pw[i, :, :])
+        adj_powers[i+1, :, :] = torch.spmm(adj, adj_powers[i, :, :])
 
-    # AdaPageRankII
+    # AdaGPR
     model = AdaGPR(nfeat=features.shape[1],
                                          nlayers=args.layer,
                                          nhidden=args.hidden,
@@ -95,7 +91,7 @@ def train(datastr,splitstr):
                                          alpha=args.alpha,
                                          variant=args.variant,
                                          gpr_coeff=gpr_coeff,
-                                         pw=pw,
+                                         adj_powers=adj_powers,
                                          device_id=cudaid).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr,
@@ -104,8 +100,8 @@ def train(datastr,splitstr):
     bad_counter = 0
     best = 999999999
     for epoch in range(args.epochs):
-        loss_tra,acc_tra = train_step(model,optimizer,features,labels,adj,idx_train)
-        loss_val,acc_val = validate_step(model,features,labels,adj,idx_val)
+        loss_tra,acc_tra = train_step(model,optimizer,features,labels,idx_train)
+        loss_val,acc_val = validate_step(model,features,labels,idx_val)
         if(epoch+1)%1 == 0: 
             print('Epoch:{:04d}'.format(epoch+1),
                 'train',
@@ -124,22 +120,11 @@ def train(datastr,splitstr):
         if bad_counter == args.patience:
             break
 
-    '''
-    l = np.zeros([args.layer,args.telportParam])
-    l1 = np.zeros([args.layer, args.telportParam])
-    #L0 = list()
-    #L = list()
-    for i in range(args.layer):
-        #print(model.convs[i].pg_coeffs.detach().numpy().T)
-        l[i,:] = model.convs[i].cf.cpu().detach().numpy()[:].T
-        #L0.append(str(model.convs[i].cf.detach().numpy()[:].T).replace(' ', ' & ').replace('[','').replace(']','').replace('\n','') )
-    print(l)
-    #print(L0)
-    '''
 
-    acc = test_step(model,features,labels,adj,idx_test)[1]
 
-    return acc*100,l
+    acc = test_step(model,features,labels,idx_test)[1]
+
+    return acc*100
 
 t_total = time.time()
 acc_list = []
@@ -147,13 +132,8 @@ cfs_list = []
 for i in range(10):
     datastr = args.data
     splitstr = 'splits/'+args.data+'_split_0.6_0.2_'+str(i)+'.npz'
-    accs,l = train(datastr, splitstr)
-    acc_list.append(accs)
-    cfs_list.append(l)
+    accs = train(datastr, splitstr)
     print(i,": {:.2f}".format(acc_list[-1]))
 print("Train cost: {:.4f}s".format(time.time() - t_total))
 print("Test acc.:{:.2f}".format(np.mean(acc_list)))
-filename = './full_supervsied_GPR_sparsemax_exp/texas_GPR_sparsemax_exp_acc_' + '_' + str(
-    args.seed) + '_' + str(args.layer) + '_' + str(args.hidden) + '_' + str(args.alpha)  + '_' + str(args.lamda) + '_' + str(args.dropout)  + '_' + str(args.lr)  + '_' + str(args.weight_decay) + '_'  + str(telportParam) + '_.mat'
-# filename = 'sims.mat'
-sio.savemat(filename, {'res': acc_list, 'cfs' : cfs_list})
+
